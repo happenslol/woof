1ad749013f124af0390526d210bd2d06777585f6
Hilmar Wiegand <me@hwgnd.de>
2025-06-05 21:24:38 +0200

    Implement toml messages format

--- README.md
+++ README.md
@@ -1,15 +1,102 @@
 # Woof
 
-Experimenting with codegen for translations
+`woof` is a simple code generator for translations.
 
-## Notes
+## How it works
 
-- Inspired by inlang/paraglide-js
-  - Codegen is a great idea, but they use a binary translation file
-  - Can we do the same, but have a stupid simple, version control friendly, text editable file format?
-- Is tree-shaking essential?
-  - Nested keys that are called as `nested.key()` are not possible if we want tree-shaking
-  - inlang generates files that export `*` from the other translation files, nested keys are exported as string literals and have to be called as `m['nested.key']()`
-- Functions are needed because the locale switching has to happen inside the translation function
-- We could allow switching between JS and react mode, in react mode we just use fragments to allow interpolating components into translations
-  - Do we need to provide components for translations? e.g. `<nested.key.C value="v" />`?
+`woof` takes `.toml` files as input and generates typescript files with functions than you can call to get your translation strings.
+
+All string interpolation is typesafe and the resulting code is tree-shakable, so you'll only ship the translations you're actually using. Lazy-loading can be taken care of by your bundler, in case you have a lot of translations.
+
+## Usage
+
+Here's what the translation files look like:
+
+```toml
+title = "My Website"
+description = "This is a description"
+
+[about]
+title = "About"
+description = "More details about this website"
+
+[about.more]
+copyright = "Copyright {year:number} by {author}"
+```
+
+With the files `locales/en.toml` and `locales/de.toml`, running `woof -o messages ./locales` will generate the following:
+
+```typescript
+// messages/index.ts
+let _locale = "en"
+
+export const setLocale = (l: "en" | "de") => (_locale = l)
+export const locale = () => _locale
+
+export * as m from "./root"
+
+// messages/root.ts
+import { locale } from "."
+
+export const title = () => {
+  const l = locale()
+  if (l === "en") return "My Website"
+  if (l === "de") return "Meine Webseite"
+  return "title"
+}
+
+export const description = () => {
+  const l = locale()
+  if (l === "en") return "This is a description"
+  if (l === "de") return "Dies ist eine Beschreibung"
+  return "description"
+}
+
+export * as about from "./about"
+
+// messages/about/index.ts
+import { locale } from ".."
+
+export const title = () => {
+  const l = locale()
+  if (l === "en") return "About"
+  if (l === "de") return "Über"
+  return "about.title"
+}
+
+export const description = () => {
+  const l = locale()
+  if (l === "en") return "More details about this website"
+  if (l === "de") return "Mehr Details über diese Webseite"
+  return "about.description"
+}
+
+export * as more from "./more"
+
+// messages/about/more/index.ts
+import { locale } from "../.."
+
+export const copyright = (year: number, author: string) => {
+  const l = locale()
+  if (l === "en") return `Copyright ${year} by ${author}`
+  if (l === "de") return `Copyright ${year} von ${author}`
+  return `about.more.copyright (year:${year}, author:${author})`
+}
+```
+
+You can then use the translations in your code like this:
+
+```typescript
+import { m } from './messages'
+
+console.log(m.about.title()) // "About"
+console.log(m.about.more.copyright({ year: 2022, author: 'me' })) // "Copyright 2022 by me"
+```
+
+You can also directory import messages from a sub-directory:
+
+```typescript
+import * as m from './messages/about/more'
+
+console.log(m.copyright({ year: 2022, author: 'me' })) // "Copyright 2022 by me"
+```
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,147 @@
-use std::{
-  collections::BTreeMap,
-  error::Error,
-  fs::{File, create_dir_all},
-  io::{Read, Write},
-  sync::LazyLock,
-};
+use clap::Parser;
+use std::collections::{BTreeMap, HashMap};
+use std::fs;
+use std::path::{Path, PathBuf};
+use thiserror::Error;
+use toml::{Table, Value};
 
-use regex::Regex;
+type Result<T> = std::result::Result<T, WoofError>;
 
-#[derive(Debug, Clone)]
-enum Entry<'a> {
-  String(&'a str),
-  Object(BTreeMap<&'a str, Entry<'a>>),
+#[derive(Error, Debug)]
+pub enum WoofError {
+  #[error("IO error: {0}")]
+  Io(#[from] std::io::Error),
+
+  #[error("Path `{0}` is not a directory")]
+  InvalidInputDirectory(String),
+
+  #[error("Output file already exists at `{0}`")]
+  OutputFileExists(PathBuf),
+
+  #[error("Root of TOML file is not a table")]
+  RootNotTable,
+
+  #[error("Invalid TOML: {0}")]
+  Toml(#[from] toml::de::Error),
+
+  #[error("Unsupported value type `{typename}` at path `{path}`")]
+  UnsupportedValueType { typename: String, path: String },
+
+  #[error("Invalid interpolation format in string: {0}")]
+  InvalidInterpolation(String),
+
+  #[error("Interpolation type mismatch between locales")]
+  InterpolationTypeMismatch,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-  let mut f = File::open("./example.locale")?;
-  let mut str = String::new();
-  f.read_to_string(&mut str)?;
-  let mut values = BTreeMap::new();
+#[derive(Debug)]
+struct Interpolation {
+  typename: Option<String>,
+  ranges: HashMap<String, (usize, usize)>,
+}
 
-  for line in str.lines() {
-    if line.trim().is_empty() {
-      continue;
-    }
+#[derive(Debug)]
+struct Message {
+  value: HashMap<String, String>,
+  interpolations: HashMap<String, Interpolation>,
+}
 
-    if line.starts_with("#") {
-      continue;
-    }
-
-    let mut parts = line.split("=");
-    let Some(key) = parts.next() else {
-      eprintln!("Invalid line: {line}");
-      continue;
-    };
-
-    let Some(value) = parts.next() else {
-      eprintln!("Invalid line: {line}");
-      continue;
-    };
-
-    insert_entry(&mut values, key, value);
+fn collect_locales<P: AsRef<Path>>(dir: P) -> Result<HashMap<String, Value>> {
+  let dir = dir.as_ref();
+  let mut result = HashMap::new();
+  if !dir.is_dir() {
+    return Err(WoofError::InvalidInputDirectory(dir.display().to_string()));
   }
 
-  create_dir_all("./out")?;
-  let mut out = File::create("./out/translations.ts")?;
+  let entries = fs::read_dir(dir)?;
+  let toml_files = entries
+    .filter_map(|e| e.ok())
+    .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some("toml"));
 
-  writeln!(out, "export const t = {{")?;
-  write_map(&mut out, &values)?;
-  writeln!(out, "}};")?;
-
-  Ok(())
-}
-
-fn insert_entry<'a>(map: &mut BTreeMap<&'a str, Entry<'a>>, key: &'a str, value: &'a str) {
-  let key = key.trim();
-  let value = value.trim();
-
-  let mut current_map = map;
-  let mut parts = key.split('.');
-
-  let mut current_key = parts.next().unwrap();
-  for part in parts {
-    current_map
-      .entry(current_key)
-      .or_insert(Entry::Object(BTreeMap::new()));
-    current_map = match current_map.get_mut(current_key).unwrap() {
-      Entry::Object(map) => map,
-      _ => unreachable!(),
+  for entry in toml_files {
+    let path = entry.path();
+    let Some(stem) = path.file_stem().and_then(|s| s.to_str()) else {
+      continue;
     };
-    current_key = part;
+
+    let stem = stem.to_string();
+    let contents = fs::read_to_string(path)?;
+    result.insert(stem.to_string(), toml::from_str(&contents)?);
   }
 
-  current_map.insert(current_key, Entry::String(value));
+  Ok(result)
 }
 
-fn write_map(out: &mut File, map: &BTreeMap<&str, Entry>) -> Result<(), Box<dyn Error>> {
-  for (key, value) in map {
+#[derive(Debug)]
+struct Module {
+  messages: BTreeMap<String, Message>,
+  modules: BTreeMap<String, Module>,
+}
+
+fn build_modules(locales: HashMap<String, Value>) -> Result<Module> {
+  let mut messages = BTreeMap::new();
+  let mut modules = BTreeMap::new();
+
+  for (locale, value) in locales {
+    let Value::Table(root) = value else {
+      return Err(WoofError::RootNotTable);
+    };
+
+    build_module(&locale, root, &mut messages, &mut modules)?;
+  }
+
+  Ok(Module { messages, modules })
+}
+
+fn build_module(
+  locale: &str,
+  table: Table,
+  messages: &mut BTreeMap<String, Message>,
+  modules: &mut BTreeMap<String, Module>,
+) -> Result<()> {
+  for (key, value) in table {
     match value {
-      Entry::String(value) => write_key(out, key, value)?,
-      Entry::Object(map) => {
-        writeln!(out, "{key}: {{")?;
-        write_map(out, map)?;
-        writeln!(out, "}},")?;
+      Value::String(s) => {
+        let interpolations = parse_interpolations(&s)?;
+        let message = messages.entry(key.clone()).or_insert_with(|| Message {
+          value: HashMap::new(),
+          interpolations: HashMap::new(),
+        });
+
+        message.value.insert(locale.to_string(), s.clone());
+        for interpolation in interpolations {
+          let entry = message
+            .interpolations
+            .entry(interpolation.name)
+            .or_insert_with(|| Interpolation {
+              typename: interpolation.typename.clone(),
+              ranges: HashMap::new(),
+            });
+
+          entry
+            .ranges
+            .insert(locale.to_string(), (interpolation.start, interpolation.end));
+
+          if interpolation.typename != entry.typename {
+            return Err(WoofError::InterpolationTypeMismatch);
+          }
+        }
+      }
+
+      Value::Table(table) => {
+        let module = modules.entry(key.clone()).or_insert_with(|| Module {
+          messages: BTreeMap::new(),
+          modules: BTreeMap::new(),
+        });
+
+        build_module(locale, table, &mut module.messages, &mut module.modules)?;
+      }
+
+      _ => {
+        return Err(WoofError::UnsupportedValueType {
+          path: key.clone(),
+          typename: value.type_str().to_string(),
+        });
       }
     }
   }
@@ -90,27 +149,118 @@ fn write_map(out: &mut File, map: &BTreeMap<&str, Entry>) -> Result<(), Box<dyn 
   Ok(())
 }
 
-static VALUE_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\{([^}]+)\}").unwrap());
+struct ParsedInterpolation {
+  typename: Option<String>,
+  name: String,
+  start: usize,
+  end: usize,
+}
 
-fn write_key(out: &mut File, key: &str, value: &str) -> Result<(), Box<dyn Error>> {
-  if value.contains('{') {
-    writeln!(out, "{key}: (values: {{")?;
+fn parse_interpolations(s: &str) -> Result<Vec<ParsedInterpolation>> {
+  let mut result = Vec::new();
+  let chars = s.chars().enumerate();
 
-    let captures = VALUE_REGEX.captures_iter(value);
-    for capture in captures {
-      let Some(key) = capture.get(1) else {
-        continue;
-      };
+  let mut parsing_interpolation = false;
+  let mut start = 0;
+  let mut parsing_type = false;
+  let mut current_name = String::new();
+  let mut current_type = String::new();
 
-      let key = key.as_str();
-      write!(out, "{key}: string,")?;
+  for (index, c) in chars {
+    if c == '{' {
+      start = index;
+      parsing_interpolation = true;
+      continue;
     }
 
-    let repl = VALUE_REGEX.replace_all(value, "$${values.$1}");
-    writeln!(out, "}}) => `{repl}`,")?;
-    return Ok(());
+    if !parsing_interpolation {
+      continue;
+    }
+
+    // Skip escaped '{'
+    if c == '{' && current_name.is_empty() {
+      parsing_interpolation = false;
+      continue;
+    }
+
+    if c == ':' {
+      if current_name.is_empty() {
+        return Err(WoofError::InvalidInterpolation(s.to_string()));
+      }
+
+      parsing_type = true;
+      continue;
+    }
+
+    if c == '}' {
+      if current_name.is_empty() {
+        return Err(WoofError::InvalidInterpolation(s.to_string()));
+      }
+
+      let typename = if !current_type.is_empty() {
+        let typename = current_type.clone();
+        current_type.clear();
+        Some(typename)
+      } else {
+        None
+      };
+
+      result.push(ParsedInterpolation {
+        name: current_name.clone(),
+        start,
+        end: index,
+        typename,
+      });
+
+      parsing_interpolation = false;
+      parsing_type = false;
+      current_name.clear();
+      continue;
+    }
+
+    if parsing_type {
+      current_type.push(c);
+      continue;
+    }
+
+    current_name.push(c);
   }
 
-  writeln!(out, "{key}: () => `{value}`,")?;
+  if parsing_interpolation {
+    return Err(WoofError::InvalidInterpolation(s.to_string()));
+  }
+
+  Ok(result)
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  /// Output directory for generated files
+  #[arg(short, long, default_value = "messages")]
+  out: String,
+
+  /// Input directory containing translation files
+  input_dir: String,
+}
+
+fn main() -> Result<()> {
+  let config = Args::parse();
+  let locales = collect_locales(config.input_dir)?;
+  let modules = build_modules(locales)?;
+
+  println!("{:#?}", modules);
+
+  let out = Path::new(&config.out);
+  if out.is_file() {
+    return Err(WoofError::OutputFileExists(out.to_path_buf()));
+  }
+
+  // if out.exists() {
+  //   fs::remove_dir_all(out)?;
+  // }
+  //
+  // fs::create_dir_all(out)?;
+
   Ok(())
 }
